09ab4c28de89a5fead418bbaf93f574baed80f68
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 02:30:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,4 +19,12 @@ class Student
         SQL
     DB[:conn].execute(sql)
   end
+
+  def self.drop_table
+    sql =  <<-SQL
+      DROP TABLE students
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 end
