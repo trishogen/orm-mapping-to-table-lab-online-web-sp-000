7059857a9c6fbc93b49d1cac7ccf0c3131b1d304
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 02:29:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,15 @@ class Student
     @name = name
     @grade = grade
   end
+
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+        )
+        SQL
+    DB[:conn].execute(sql)
+  end
 end
